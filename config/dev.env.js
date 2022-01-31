2e579be25f42b345a47c9d60fc24105ccd02bd1f
xiaoxiao-12138 <2642562640@qq.com>
2022-01-31 17:58:12 +0800

    服务器运行初版

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8888"',
+  BASE_API: '"http://120.25.144.146:8888"',
   OSS_PATH: '"http://hsunnyc.oss-cn-beijing.aliyuncs.com"'
 })
